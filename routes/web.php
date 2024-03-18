b44f2e8dbe32475d10144d73a819642e5ffb9db5
Hansal Scripts <65434536+hansalscripts@users.noreply.github.com>
2024-03-18 21:45:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,24 @@ use Symfony\Component\Finder\Finder;
 
 Route::middleware('web')->group(function () {
     $livewireNamespace = config('livewire.class_namespace');
-    $livewirePath = base_path('app/' . str_replace('\\', '/', $livewireNamespace));
+    $path = base_path(str_replace('\\', DIRECTORY_SEPARATOR , $livewireNamespace));
 
-    if (!is_dir($livewirePath)) {
+    $namespace = app()->getNamespace();
+
+    if (!is_dir($path)) {
         return;
     }
 
-    $finder = new Finder();
-    $finder->files()->in($livewirePath)->name('*.php');
+    foreach ((new Finder)->in($path) as $component) {
+        $component = $namespace . str_replace(
+                ['/', '.php'],
+                ['\\', ''],
+                Str::after($component->getRealPath(), realpath(app_path()) . DIRECTORY_SEPARATOR)
+            );
 
-    foreach ($finder as $file) {
-        $relativePath = $file->getRelativePathname();
-        $className = $livewireNamespace . '\\' . str_replace('/', '\\', Str::before($relativePath, '.php'));
-
-        if (is_subclass_of($className, \Livewire\Component::class)) {
-            if (method_exists($className, 'route')) {
-                app()->call([$className, 'route']);
-            }
+        if (method_exists($component, 'route')) {
+            app($component)->route()->uses($component);
         }
     }
+
 });
